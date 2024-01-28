5f02ca7440ccfd7d833be537f05ced198312b545
wrp <wang@wrp.com>
2024-01-28 23:39:56 +0800

    又不好用了

--- manager/app/build.gradle.kts
+++ manager/app/build.gradle.kts
@@ -139,14 +139,14 @@ dependencies {
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
-    implementation ("io.reactivex.rxjava2:rxandroid:2.0.1")
-    implementation ("io.reactivex.rxjava2:rxjava:2.1.8")
-    implementation ("com.squareup.retrofit2:retrofit:2.4.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.4.0")
-//    implementation ("com.squareup.retrofit2:converter-scalars:2.4.0")
-    implementation ("com.squareup.retrofit2:adapter-rxjava2:2.4.0")
-//    implementation ("com.squareup.okhttp3:logging-interceptor:4.10.0")
-    implementation( "com.jakewharton.rxrelay2:rxrelay:2.1.0")
+//    implementation ("io.reactivex.rxjava2:rxandroid:2.0.1")
+//    implementation ("io.reactivex.rxjava2:rxjava:2.1.8")
+//    implementation ("com.squareup.retrofit2:retrofit:2.4.0")
+//    implementation ("com.squareup.retrofit2:converter-gson:2.4.0")
+//    implementation ("com.squareup.retrofit2:adapter-rxjava2:2.4.0")
+//    implementation( "com.jakewharton.rxrelay2:rxrelay:2.1.0")
+    
+    //    implementation ("com.squareup.okhttp3:logging-interceptor:4.10.0")
 //    implementation ("com.squareup.okhttp3:okhttp:4.10.0")
 //    implementation ("com.squareup.okhttp:okhttp-urlconnection:2.2.0")
 
